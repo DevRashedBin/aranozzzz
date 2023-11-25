68c01d4ba7e3170b45441191d6d8bbc2f935fc06
Rashedul Islam <143278412+DevRashedBin@users.noreply.github.com>
2023-11-25 22:23:14 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $(".banner_slider").owlCarousel({
+        items: 1,
+        loop: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        nav: true,
+        navText: ["previous", "next"],
+        smartSpeed: 1000,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+    });
+  });
+
+//   product_list_slider
+
+$(document).ready(function(){
+    $(".product_list_slider").owlCarousel({
+        items: 1,
+        loop: true,
+        dots: false,
+        autoplay: true,
+        autoplayTimeout: 5000,
+        nav: true,
+        navText: ["previous", "next"],
+        smartSpeed: 1000,
+        animateOut: 'fadeOut',
+        animateIn: 'fadeIn',
+    });
+  });
+
+//   product_list_slider
